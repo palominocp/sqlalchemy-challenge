91eaac0c50156b1a927b33809a0b08499577b848
palominocp <palominocp@outlook.com>
2020-09-03 23:53:26 -0500

    app

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+app = Flask(__name__)
+@app.route("/")
+def home():
+    return (
+        f"Welcome to the API!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end><br/>"
+    )
+
+@app.route("/about")
+def about():
+    print("Server received request for 'About' page...")
+    return "Welcome to my 'About' page!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
