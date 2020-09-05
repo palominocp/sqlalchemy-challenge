ce3f4b7c8f81a74014af31b6681b881343f98acb
palominocp <palominocp@outlook.com>
2020-09-04 20:06:42 -0500

    app review

--- app.py
+++ app.py
@@ -1,9 +1,31 @@
-from flask import Flask
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+# Database Setup
+
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+Base.classes.keys()
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Flask Setup
+
 app = Flask(__name__)
+
+# Flask Routes
+
 @app.route("/")
 def home():
     return (
-        f"Welcome to the API!<br/>"
+        f"Welcome to my API!<br/>"
         f"Available Routes:<br/>"
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
@@ -12,10 +34,24 @@ def home():
         f"/api/v1.0/<start>/<end><br/>"
     )
 
-@app.route("/about")
-def about():
-    print("Server received request for 'About' page...")
-    return "Welcome to my 'About' page!"
+
+#Return the JSON representation of your dictionary.
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    # Create link from Python to DB
+    session = Session(engine)
+
+    #Convert the query results to a dictionary using date as the key and prcp as the value.
+    results = session.query(Measurement.date, Measurement.prcp).all()
+    session.close()
+    all_measurements = []
+    for date, prcp in results:
+        measurement_dict = {}
+        measurement_dict[date] = prcp
+        all_measurements.append(measurement_dict)
+    
+    return jsonify(all_measurements)
 
 if __name__ == "__main__":
     app.run(debug=True)
